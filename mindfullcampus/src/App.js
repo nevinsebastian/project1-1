33d7f4171ee283231aad53c955e5728d555379d1
NEVIN <nevin@pop-os.localdomain>
2023-05-22 14:14:26 +0530

    added A basic login page

--- mindfullcampus/src/App.js
+++ mindfullcampus/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import LoginForm from './components/LoginForm'
+
 
 function App() {
+  const adminUser = {
+    email: "admin@gmail.com",
+    password:"admin123"
+  }
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+      console.log("login")
+      setUser({
+        name: details.name,
+        email:details.email
+      })
+    } else {
+      console.log("details do not match")
+      setError("details do not match")
+    }
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ""});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "")? (
+        <div className='welcome'>
+          <h1>welcome, <span>{user.name}</span></h1>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ):
+      (
+        <LoginForm Login={Login} error={error} />
+      )
+      }
     </div>
   );
 }
