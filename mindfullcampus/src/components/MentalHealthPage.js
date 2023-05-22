04be9c901956ee2dcce6414649b6f1faba8e0a18
NEVIN <nevin@pop-os.localdomain>
2023-05-23 00:22:36 +0530

    added backend

---
+++ mindfullcampus/src/components/MentalHealthPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MenstrualHealthPage() {
+  return (
+    <div>
+      <h2>Menstrual Health Page</h2>
+      
+    </div>
+  );
+}
+
+export default MenstrualHealthPage;
