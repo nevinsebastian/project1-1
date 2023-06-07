823cd9be3c556f4c4af2a060e644db3ba6add5f2
NEVIN <nevin@pop-os.localdomain>
2023-06-07 16:07:43 +0530

    added phy

--- mindfullcampus/src/App.js
+++ mindfullcampus/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import Physicalhealth from './components/Physicalhelth';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
+        <Route path='/physical-health' element={<Physicalhealth />}/>
       </Routes>
     </Router>
   );
