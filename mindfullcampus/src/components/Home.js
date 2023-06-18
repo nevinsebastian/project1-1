340985c31f29c6da27e8da980987c45739ea094f
sreejith <imsreejithofficial@gmail.com>
2023-06-18 19:28:42 +0530

    frontend and backend linking

--- mindfullcampus/src/components/Home.js
+++ mindfullcampus/src/components/Home.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './home.css'; // Import the home.css file
 
 function Home() {
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className='formContainer'>
+      <h1 className='pageTitle'>Mindful Campus</h1>
       <div className="tiles">
         <Link to="/mental-health" className="tile">
-          <h3>Mental Health</h3>
-          {/* Add mental health content */}
+          <div className="card">
+            <h3>Mental Health</h3>
+            {/* Add mental health content */}
+          </div>
         </Link>
         <Link to="/physical-health" className="tile">
-          <h3>Physical Health</h3>
-          {/* Add physical health content */}
+          <div className="card">
+            <h3>Physical Health</h3>
+            {/* Add physical health content */}
+          </div>
         </Link>
         <Link to="/menstrual-health" className="tile">
-          <h3>Menstrual Health</h3>
-          {/* Add menstrual health content */}
+          <div className="card">
+            <h3>Menstrual Health</h3>
+            {/* Add menstrual health content */}
+          </div>
         </Link>
       </div>
     </div>
--- mindfullcampus/src/components/Login.js
+++ mindfullcampus/src/components/Login.js
@@ -1,29 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
-    return (
-        <div className="formContainer">
-            <div className="formWrapper">
-    
-                <span className='logo'>Mindful campus</span>
-                <span className='title'>Login</span>
-                
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-                <form>
-                    
-                    <input type='email'placeholder='email'/>
-                    <input type='password'placeholder='password'/>
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const formData = new FormData();
+      formData.append('username', email);
+      formData.append('password', password);
+  
+      const response = await axios.post('http://127.0.0.1:8000/login', formData);
+  
+      // Save the token received from the backend to local storage
+      localStorage.setItem('token', response.data.access_token);
+      
+     
+      // Redirect to the desired page (e.g., Home)
+      // You can use the useHistory hook from 'react-router-dom' for redirection
+      // Example: useHistory().push('/home');
+     navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
 
-                
-                    <button>Login</button>
-                </form>
-                <p>Don't have an account? Register</p>
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Mindful campus</span>
+        <span className="title">Login</span>
 
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-            </div>
-            
-        </div>
-    )
-}
-
-export default Login
+          <button type="submit">Login</button>
+        </form>
+        <p>Don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
+export default Login;
